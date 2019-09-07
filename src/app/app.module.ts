0e288fbb4e9347cb988aa41b9a2d6135df377be0
ClauGarfiasMx <apps@claudiagarfias.works>
2019-09-06 20:31:23 -0500

    creating components & adding router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { FormComponent } from "./form/form.component";
+import { RouterModule } from "@angular/router";
+
+const routes: Route[] = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "form", component: FormComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
